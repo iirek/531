3cb92393462ffcd9ea9bf8ca631cd46d318d6443
iirek <l2irro@gmail.com>
2016-11-01 13:34:41 +0000

    Remove some unused functions

--- import_tool.py
+++ import_tool.py
@@ -16,21 +16,11 @@ def load_data(import_file: str) -> List[Dict[str, Union[int, float]]]:
 # convert floats to Decimals
 def convert_to_expected_format(data: List[Dict[str, Union[int, float]]])-> List[Dict[str, Decimal]]:
     for cycle in data:
-        if index in cycle:
+        if 'index' in cycle:
             del cycle['index']
         for lift in cycle:
             cycle[lift] = Decimal(cycle[lift])
     return data
-
-
-def generate_sets(cycle_training_max: Dict[str,Decimal]):
-
-    pass
-
-
-def save_to_db():
-    pass
-
 
 
 if __name__ == '__main__':
